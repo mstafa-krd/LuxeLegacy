3a70d4ea30b66b971151940f810c1be4eacaf2d9
mstafa-krd <mstafamzere1@gmail.com>
2023-08-23 10:29:36 +0300

    index

--- src/index.js
+++ src/index.js
@@ -1,20 +1,25 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 import WindowContext from "./Context/WindowContext";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import CartProvider from "./Context/CartContext";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <CartProvider>
-
-  <WindowContext>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </WindowContext>
-  </CartProvider>
+  <React.StrictMode>
+    <CartProvider>
+      <WindowContext>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </WindowContext>
+    </CartProvider>
+  </React.StrictMode>
 );
+
+reportWebVitals();
